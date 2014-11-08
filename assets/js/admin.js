3aac11a274b75ca166b585aa4cca8d784ae3c441
Takahara Shujin <takaharashujin@gmail.com>
2014-11-08 19:37:23 +0200

    Assets, Bootstrap, Vendor Folder Upload

---
+++ assets/js/admin.js
@@ -0,0 +1,10 @@
+﻿jQuery.noConflict();
+jQuery(document).ready(function($) {
+	$("[data-toggle='tooltip']").tooltip({
+		container : 'body',
+		placement : 'top'
+	});
+	
+
+	$('.wysi').wysihtml5();
+});
